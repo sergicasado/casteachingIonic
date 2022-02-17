c3fc55f22e5574b1ea9ea74bd6529469c12d1cc1
sergicasado <sergicasado1@iesebre.com>
2022-02-17 15:47:48 +0100

    wip

--- src/views/Video.vue
+++ src/views/Video.vue
@@ -5,7 +5,7 @@
         <ion-buttons slot="start">
           <ion-menu-button color="primary"></ion-menu-button>
         </ion-buttons>
-        <ion-title>{{ video.title }}</ion-title>
+        <ion-title>Video {{ $route.params.id }}</ion-title>
       </ion-toolbar>
     </ion-header>
 
@@ -17,40 +17,18 @@
       </ion-header>
 
       <div id="container">
-        <iframe id="video" :src="video.url" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        <ion-card>
-          <ion-card-header>
-            <ion-card-title>{{ video.title }}</ion-card-title>
-            <ion-card-subtitle>{{ video.published_at}}</ion-card-subtitle>
-          </ion-card-header>
+        <strong class="capitalize"> Video {{ $route.params.id }}</strong>
 
-          <ion-card-content>
-            {{ video.description }}
-          </ion-card-content>
-        </ion-card>
+        <p> TODO VIDEO</p>
       </div>
     </ion-content>
   </ion-page>
 </template>
 
 <script>
-
-import {
-  IonButtons,
-  IonCard,
-  IonCardHeader,
-  IonCardSubtitle,
-  IonCardTitle,
-  IonCardContent,
-  IonContent,
-  IonHeader,
-  IonMenuButton,
-  IonPage,
-  IonTitle,
-  IonToolbar
-} from '@ionic/vue';
+import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
 export default {
-  name: 'Video',
+  name: 'Folder',
   components: {
     IonButtons,
     IonContent,
@@ -58,60 +36,31 @@ export default {
     IonMenuButton,
     IonPage,
     IonTitle,
-    IonToolbar,
-    IonCard,
-    IonCardHeader,
-    IonCardTitle,
-    IonCardSubtitle,
-    IonCardContent
-  },
-  data() {
-    return {
-      video: {}
-    }
-  },
-  async created() {
-    this.video = await casteaching.video.show(this.$route.params.id)
-    this.video = {
-      id: 1,
-      title: "Ubuntu 101",
-      description: "# Here description",
-      url: "https://www.youtube.com/embed/w8j07_DBl_I",
-      published_at: "2020-12-13T20:00:00.000000Z",
-    }
+    IonToolbar
   }
 }
 </script>
 
 <style scoped>
-
-#video {
-  width:100%;
-  height:70vh;
+#container {
+  text-align: center;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 50%;
+  transform: translateY(-50%);
 }
-
-/*#container {*/
-/*  text-align: center;*/
-/*  position: absolute;*/
-/*  left: 0;*/
-/*  right: 0;*/
-/*  top: 50%;*/
-/*  transform: translateY(-50%);*/
-/*}*/
-
-/*#container strong {*/
-/*  font-size: 20px;*/
-/*  line-height: 26px;*/
-/*}*/
-
-/*#container p {*/
-/*  font-size: 16px;*/
-/*  line-height: 22px;*/
-/*  color: #8c8c8c;*/
-/*  margin: 0;*/
-/*}*/
-
-/*#container a {*/
-/*  text-decoration: none;*/
-/*}*/
+#container strong {
+  font-size: 20px;
+  line-height: 26px;
+}
+#container p {
+  font-size: 16px;
+  line-height: 22px;
+  color: #8c8c8c;
+  margin: 0;
+}
+#container a {
+  text-decoration: none;
+}
 </style>
